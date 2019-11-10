bb8fc579c622a8bd8c01f4f89d34f9d0de6512c6
jonathan brierre <jonathanbrierre@yahoo.com>
2019-11-10 02:48:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ function deepestChild(){
 }
 
 function increaseRankBy(n) {
-  const list = document.querySelectorAll('ul.ranked-list').children
+  const list = document.querySelectorAll('ul.ranked-list')[0].children
   for (let i =0;i<list.length;i++){
     list[i].innerHTML = (parseInt(list[i].innerHTML) + n).toString()
   }
   
-  // const list2 = document.querySelectorAll('ul.ranked-list')[1].children
-  // for (let i =0;i<list.length;i++){
-  //   list2[i].innerHTML = (parseInt(list2[i].innerHTML) + n).toString()
-  // }
+   const list2 = document.querySelectorAll('ul.ranked-list')[1].children
+  for (let i =0;i<list.length;i++){
+    list2[i].innerHTML = (parseInt(list2[i].innerHTML) + n).toString()
+  }
   
 }
