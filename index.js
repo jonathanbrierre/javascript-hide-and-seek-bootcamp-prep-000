f575beb44d6a3fef325ac325192323c27678a3cb
jonathan brierre <jonathanbrierre@yahoo.com>
2019-11-10 02:53:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  const dil = document.querySelector( `div.target`);
+  return dil
+}
+
+
+function deepestChild(){
+  
+  const duck = document.querySelector('#grand-node div div div div ')
+  
+  return duck
+    
+}
+
+function increaseRankBy(n) {
+  const list = document.querySelectorAll('ul.ranked-list')[0].children
+  for (let i =0;i<list.length;i++){
+    list[i].innerHTML = (parseInt(list[i].innerHTML) + n).toString()
+  }
+  
+   const list2 = document.querySelectorAll('ul.ranked-list')[1].children
+  for (let i =0;i<list2.length;i++){
+    list2[i].innerHTML = (parseInt(list2[i].innerHTML) + n).toString()
+  }
+  
+}
